a8b2c7d9112662ccc76695aa1fb688f1b9a64dcb
Matt Dean <this.matt.dean@gmail.com>
2024-08-26 19:48:20 -0400

    renders hello world

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+const App = () => {
+  return (
+    <>
+      <Box>
+        <Typography>Hello World</Typography>
+      </Box>
+    </>
+  )
+}
+
+export default App
