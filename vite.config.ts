8421a88d7ddeb8cb62e1cbcc9663751aded167a7
Matt Dean <this.matt.dean@gmail.com>
2024-08-26 19:43:40 -0400

    moves index file location

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
   plugins: [react()],
+  publicDir: './public',
   server: {
     port: 3000,
   },
-});
+})
