45201adae7a2382ce4cb8afeb5a85915939bd0d7
Matt Dean <this.matt.dean@gmail.com>
2024-08-30 19:40:21 -0400

    testing pages deploy

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -26,6 +26,8 @@ const Body = () => {
   const filteredData: PlayerPositionCollection[][] = useMemo(() => {
     const stage_1: PlayerPositionCollection[] = []
 
+    console.log('test')
+
     data.forEach((p) => {
       const foundCollection = stage_1.find((c) => c.position === p.pos)
 
