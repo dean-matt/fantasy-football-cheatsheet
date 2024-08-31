2eff8e94f9c1ec665b547bd9fad21cc434ee4688
Matt Dean <this.matt.dean@gmail.com>
2024-08-30 20:12:27 -0400

    adjusts config to properly route to assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fantasy-football-cheatsheet/',
   build: {
     emptyOutDir: true,
     outDir: '../dist',
   },
   plugins: [react()],
-  publicDir: 'src/assets',
+  publicDir: './assets',
   root: 'src',
   server: {
     port: 3000,
